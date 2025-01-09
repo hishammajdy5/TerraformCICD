727b27c57b1bbdea527fe90bd27bd9ea408b7375
hishammajdy5 <hisham.majdy5@gmail.com>
2025-01-09 11:40:00 +0200

    push 2 storage accounts on prod sub

--- main.tf
+++ main.tf
@@ -42,3 +42,11 @@ resource "azurerm_storage_account" "storageaccount" {
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "stg${random_string.uniquestring.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
