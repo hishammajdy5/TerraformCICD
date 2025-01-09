22a44b34eeb44190530c96510476b9ab59751a65
hishammajdy5 <hisham.majdy5@gmail.com>
2025-01-09 11:51:04 +0200

    push 2 GRS and LRS storage acc on prod

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "azurerm_resource_group" "rg" {
   location = "eastus"
 }
 
-resource "azurerm_storage_account" "storageaccount" {
+resource "azurerm_storage_account" "storageaccount2" {
   name                     = "stg${random_string.uniquestring.result}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
@@ -43,11 +43,11 @@ resource "azurerm_storage_account" "storageaccount" {
   account_replication_type = "GRS"
 }
 
-/*
-resource "azurerm_storage_account" "storageaccount" {
+
+resource "azurerm_storage_account" "storageaccount1" {
   name                     = "stg${random_string.uniquestring.result}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-}*/
+}
